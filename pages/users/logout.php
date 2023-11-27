38e92f42e6c232c08b3473d5a1b9a00d51d6a4e0
kholoud001 <kholoud.sanak@gmail.com>
2023-11-27 15:05:51 +0100

    user logout

---
+++ pages/users/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to signin.php after logout
+header("Location: signin.php");
+exit();
+
